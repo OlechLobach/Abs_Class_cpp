ad6f8f5442bfcae6d56567fc4778dec616eb6492
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2024-01-23 20:02:23 +0200

    initiall commit

--- abstract.h
+++ abstract.h
@@ -1,6 +1,123 @@
 #ifndef ABSTRACT_H
 #define ABSTRACT_H
 
+#include <iostream>
+using namespace std;
+
+namespace Lists {
+
+   
+    class List {
+    public:
+        virtual void insert(int value) = 0;
+        virtual int remove() = 0;
+        virtual ~List() {}
+    };
+
+  
+    class Stack : public List {
+    private:
+        class Node {
+        public:
+            int data;
+            Node* next;
+
+            Node(int value) : data(value), next(nullptr) {}
+        };
+
+        Node* top;
+
+    public:
+        Stack() : top(nullptr) {}
+
+        void insert(int value) override {
+            Node* newNode = new Node(value);
+            newNode->next = top;
+            top = newNode;
+        }
+
+        int remove() override {
+            if (isEmpty()) {
+                cerr << "Stack is empty. Cannot remove element.\n";
+                return -1;
+            }
+
+            int removedValue = top->data;
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+
+            return removedValue;
+        }
+
+        bool isEmpty() const {
+            return top == nullptr;
+        }
+
+        ~Stack() {
+            while (!isEmpty()) {
+                remove();
+            }
+        }
+    };
 
 
-#endif
+    class Queue : public List {
+    private:
+        class Node {
+        public:
+            int data;
+            Node* next;
+
+            Node(int value) : data(value), next(nullptr) {}
+        };
+
+        Node* front;
+        Node* rear;
+
+    public:
+        Queue() : front(nullptr), rear(nullptr) {}
+
+        void insert(int value) override {
+            Node* newNode = new Node(value);
+            if (isEmpty()) {
+                front = rear = newNode;
+            }
+            else {
+                rear->next = newNode;
+                rear = newNode;
+            }
+        }
+
+        int remove() override {
+            if (isEmpty()) {
+                cerr << "Queue is empty. Cannot remove element.\n";
+                return -1;
+            }
+
+            int removedValue = front->data;
+            Node* temp = front;
+            front = front->next;
+
+            if (front == nullptr) {
+                rear = nullptr;
+            }
+
+            delete temp;
+
+            return removedValue;
+        }
+
+        bool isEmpty() const {
+            return front == nullptr;
+        }
+
+        ~Queue() {
+            while (!isEmpty()) {
+                remove();
+            }
+        }
+    };
+}
+
+#endif 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include"abstract.h"
 
-int main()
-{
+int main() {
+   
+    using namespace Lists;
 
+    List* myList = nullptr;
 
-	return 0;
+    cout << "Enter 0 for Stack, 1 for Queue: ";
+    int choice;
+    cin >> choice;
+
+    switch (choice) {
+    case 0:
+        myList = new Stack();
+        break;
+    case 1:
+        myList = new Queue();
+        break;
+    default:
+        cerr << "Invalid choice. Exiting program.\n";
+        return 1;
+    }
+
+    myList->insert(10);
+    myList->insert(20);
+    myList->insert(30);
+
+    cout << "Removed: " << myList->remove() << endl;
+    cout << "Removed: " << myList->remove() << endl;
+
+    delete myList;
+
+    return 0;
 }
